7965cb424f8fe5bccb124b4e3478d1a0384a530f
Dervak <federagone@gmail.com>
2022-11-23 23:48:57 -0300

    no se si anda

---
+++ Files/FilesManager.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+
+class Container {
+    constructor(path, filename) {
+        this.path = path;
+        this.filename = filename;
+        this.codification = "utf-8";
+    }
+    save = (product) => {
+        let fileData = []
+        filename = `${__dirname}${this.path}/${this.filename}`
+        fs.readFile(filename, this.codification, (err, data) => {
+            if (err) throw new Error(err)
+            fileData = JSON.parse(data)
+        })
+        fileData.push({
+            id: fileData.length++,
+            ...product
+        })
+        fs.writeFile(filename, JSON.stringify(fileData))
+    }
+    getById = (id) => {
+        let fileData = []
+        filename = `${__dirname}${this.path}/${this.filename}`
+        fs.readFile(filename, this.codification, (err, data) => {
+            if (err) throw new Error(err)
+            fileData = JSON.parse(data)
+        })
+        fileData.filter(product => {
+            return product.id === id
+        })
+        return fileData
+    }
+    getAll = () => {
+        let fileData = []
+        filename = `${__dirname}${this.path}/${this.filename}`
+        fs.readFile(filename, this.codification, (err, data) => {
+            if (err) throw new Error(err)
+            fileData = JSON.parse(data)
+        })
+        return fileData
+    }
+    deleteById = (id) => {
+        let fileData = []
+        filename = `${__dirname}${this.path}/${this.filename}`
+        fs.readFile(filename, this.codification, (err, data) => {
+            if (err) throw new Error(err)
+            fileData = JSON.parse(data)
+        })
+        fileData.filter(product => {
+            return product.id !== id
+        })
+        fs.writeFile(filename, JSON.stringify(fileData))
+    }
+    deleteAll = () => {
+        filename = `${__dirname}${this.path}/${this.filename}`
+        fs.writeFile(filename, JSON.stringify([]))
+    }
+}
+Container("./", "products.txt")
